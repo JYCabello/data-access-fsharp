ab1a860c23f9d24242d4fea1885a9cfad523e03e
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-28 11:56:20 +0200

    setup

--- src/DataAccessFSharp/DataAccessFSharp/Library.fs
+++ src/DataAccessFSharp/DataAccessFSharp/Library.fs
@@ -1,5 +1,2 @@
-﻿namespace DataAccessFSharp
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+﻿let connstring =
+  "Server=localhost,1433;Database=datafsharp;User Id=sa;Password=abcdABCD1234;"
--- src/DataAccessFSharp/Migrations/Program.fs
+++ src/DataAccessFSharp/Migrations/Program.fs
@@ -1,5 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open System
+﻿open System
 open System.Reflection
 open DbUp
 
