0e2a4c90b9a6f155d04e1f03076b7b3027a32a8b
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-28 12:54:16 +0200

    data!

--- src/DataAccessFSharp/DataAccessFSharp/Library.fs
+++ src/DataAccessFSharp/DataAccessFSharp/Library.fs
@@ -1,2 +1,36 @@
-﻿let connstring =
+﻿open System
+open System.Data.SqlClient
+open Dapper
+
+[<CLIMutable>]
+type Person = { ID: int; LastName: string; FirstName: string }
+
+[<CLIMutable>]
+type PersonInsert = { LastName: string; FirstName: string }
+
+let connstring =
   "Server=localhost,1433;Database=datafsharp;User Id=sa;Password=abcdABCD1234;"
+
+
+let conn = new SqlConnection(connstring)
+
+let anonInsertSql = "INSERT INTO People (LastName, FirstName) VALUES (@a, @b)"
+
+conn
+  .Execute(
+    anonInsertSql,
+    {| A = Guid.NewGuid().ToString(); B = Guid.NewGuid().ToString() |})
+|> ignore
+
+let typedInsertSql = "INSERT INTO People (LastName, FirstName) VALUES (@lastName, @firstName)"
+
+conn
+  .Execute(
+    typedInsertSql,
+    { LastName = Guid.NewGuid().ToString(); FirstName = Guid.NewGuid().ToString() })
+|> ignore
+
+let sql = "SELECT TOP 6 ID, LastName, FirstName FROM People ORDER BY ID DESC"
+let people = conn.Query<Person> sql
+
+people |> Seq.iter Console.WriteLine 
