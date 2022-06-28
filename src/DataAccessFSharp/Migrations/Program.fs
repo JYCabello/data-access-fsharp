db0d1e62c1e98c737e95a3c23468df67770f8687
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-28 11:48:39 +0200

    dbup

--- src/DataAccessFSharp/Migrations/Program.fs
+++ src/DataAccessFSharp/Migrations/Program.fs
@@ -1,2 +1,22 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System
+open System.Reflection
+open DbUp
+
+let connstring =
+  "Server=localhost,1433;Database=datafsharp;User Id=sa;Password=abcdABCD1234;"
+
+let upgrader =
+  DeployChanges
+    .To
+    .SqlDatabase(connstring)
+    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+    .LogToConsole()
+    .Build()
+
+let result = upgrader.PerformUpgrade()
+
+match result.Successful with
+| true -> "All good"
+| false -> result.Error.Message
+|> Console.WriteLine
