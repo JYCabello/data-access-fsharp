036bd9340cb9ae175cdc02bbc8d21b87ac98c1b3
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-28 14:23:58 +0200

    done

--- src/DataAccessFSharp/DataAccessFSharp/Library.fs
+++ src/DataAccessFSharp/DataAccessFSharp/Library.fs
@@ -3,7 +3,10 @@ open System.Data.SqlClient
 open Dapper
 
 [<CLIMutable>]
-type Person = { ID: int; LastName: string; FirstName: string }
+type Person =
+  { ID: int
+    LastName: string
+    FirstName: string }
 
 [<CLIMutable>]
 type PersonInsert = { LastName: string; FirstName: string }
@@ -11,26 +14,28 @@ type PersonInsert = { LastName: string; FirstName: string }
 let connstring =
   "Server=localhost,1433;Database=datafsharp;User Id=sa;Password=abcdABCD1234;"
 
-
 let conn = new SqlConnection(connstring)
 
 let anonInsertSql = "INSERT INTO People (LastName, FirstName) VALUES (@a, @b)"
 
-conn
-  .Execute(
-    anonInsertSql,
-    {| A = Guid.NewGuid().ToString(); B = Guid.NewGuid().ToString() |})
+conn.Execute(
+  anonInsertSql,
+  {| A = Guid.NewGuid().ToString()
+     B = Guid.NewGuid().ToString() |}
+)
 |> ignore
 
-let typedInsertSql = "INSERT INTO People (LastName, FirstName) VALUES (@lastName, @firstName)"
+let typedInsertSql =
+  "INSERT INTO People (LastName, FirstName) VALUES (@lastName, @firstName)"
 
-conn
-  .Execute(
-    typedInsertSql,
-    { LastName = Guid.NewGuid().ToString(); FirstName = Guid.NewGuid().ToString() })
+conn.Execute(
+  typedInsertSql,
+  { LastName = Guid.NewGuid().ToString()
+    FirstName = Guid.NewGuid().ToString() }
+)
 |> ignore
 
 let sql = "SELECT TOP 6 ID, LastName, FirstName FROM People ORDER BY ID DESC"
 let people = conn.Query<Person> sql
 
-people |> Seq.iter Console.WriteLine 
+people |> Seq.iter Console.WriteLine
